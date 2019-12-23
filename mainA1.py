82d0f9b505bdab781d26844430f73e84d0e47e8b
pecarval <pedro_oliveira97@outlook.pt>
2019-12-23 16:00:19 +0000

    Add scripts to run all models for each A task separately

---
+++ mainA1.py
@@ -0,0 +1,64 @@
+import sys
+import numpy as np
+import pandas as pd
+
+from A1.A1FL import A1 as A1_FL
+from A1.A1LBP import A1 as A1_LBP
+from A1.A1VGG import A1 as A1_VGG
+
+from Datasets import dataA1
+
+# ======================================================================================================================
+# Data Pre-Processing
+
+dataA1_train1, dataA1_test1, lbsA1_train1, lbsA1_test1 = dataA1.mainA1Landmarks()
+dataA1_train2, dataA1_test2, lbsA1_train2, lbsA1_test2 = dataA1.mainA1LBP()
+#dataA1_3, dataset_sizes = dataA1.mainA1VGG()
+
+# ======================================================================================================================
+# Task A1 with Facial Landmarks as Feature
+
+model1_A1 = A1_FL()
+acc1_A1_train = model1_A1.train(dataA1_train1, lbsA1_train1)
+acc1_A1_test = model1_A1.test(dataA1_test1, lbsA1_test1)
+
+# Clean up memory
+del model1_A1, dataA1_train1, dataA1_test1, lbsA1_train1, lbsA1_test1
+
+#acc1_A1_train = 'TBD'
+#acc1_A1_test = 'TBD'
+
+# ======================================================================================================================
+# Task A1 with LBP as Feature
+
+model2_A1 = A1_LBP()
+acc2_A1_train = model2_A1.train(dataA1_train2, lbsA1_train2)
+acc2_A1_test = model2_A1.test(dataA1_test2, lbsA1_test2)
+
+# Clean up memory
+del model2_A1, dataA1_train2, dataA1_test2, lbsA1_train2, lbsA1_test2
+
+#acc2_A1_train = 'TBD'
+#acc2_A1_test = 'TBD'
+
+# ======================================================================================================================
+# Task A1 with pre-trained VGG model
+'''
+model3_A1 = A1_VGG()
+acc3_A1_train = model3_A1.train(dataA1_3, dataset_sizes)
+acc3_A1_test = model3_A1.test(dataA1_3)
+'''
+# Clean up memory
+#del model3_A1, dataA1_3, dataset_sizes
+
+acc3_A1_train = 'TBD'
+acc3_A1_test = 'TBD'
+
+
+# ======================================================================================================================
+## Printing results
+
+print('TA1_1:{},{};TA1_2:{},{};TA1_3:{},{};'.format(acc1_A1_train, acc1_A1_test,
+                                                    acc2_A1_train, acc2_A1_test,
+                                                    acc3_A1_train, acc3_A1_test))
+
---
+++ mainA2.py
@@ -0,0 +1,50 @@
+import sys
+import numpy as np
+import pandas as pd
+
+from A2.A2SVM import A2 as A2_SVM
+from A2.A2CNN import A2 as A2_VGG
+
+from Datasets import dataA2
+
+# ======================================================================================================================
+# Data Pre-Processing
+
+dataA2_train1, dataA2_test1, lbsA2_train1, lbsA2_test1 = dataA2.mainA2Landmarks()
+dataA2_2, dataset_sizes = dataA2.mainA2VGG()
+
+
+# ======================================================================================================================
+# Task A2 with Facial Landmarks as Feature
+
+model1_A2 = A2_SVM()
+acc1_A2_train = model1_A2.train(dataA2_train1, lbsA2_train1)
+acc1_A2_test = model1_A2.test(dataA2_test1, lbsA2_test1)
+
+# Clean up memory
+del model1_A2, dataA2_train1, dataA2_test1, lbsA2_train1, lbsA2_test1
+
+#acc1_A2_train = 'TBD'
+#acc1_A2_test = 'TBD'
+
+
+# ======================================================================================================================
+# Task A2 with pre-trained VGG model
+'''
+model2_A2 = A2_VGG()
+acc2_A2_train = model2_A2.train(dataA2_2, dataset_sizes)
+acc2_A2_test = model2_A2.test(dataA2_2)
+'''
+# Clean up memory
+#del model2_A2, dataA2_3, dataset_sizes
+
+acc2_A2_train = 'TBD'
+acc2_A2_test = 'TBD'
+
+
+# ======================================================================================================================
+## Printing results
+
+print('TA2_1:{},{};TA2_2:{},{};'.format(acc1_A2_train, acc1_A2_test,
+                                        acc2_A2_train, acc2_A2_test))
+
